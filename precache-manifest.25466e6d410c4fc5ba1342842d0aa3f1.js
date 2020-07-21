e011a10d195627cc31d5061c025dc86208fd8184
Chommy92 <chommyukatu92@gmail.com>
2020-07-21 23:25:58 +0300

    Updates

---
+++ precache-manifest.25466e6d410c4fc5ba1342842d0aa3f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec45917a410d5835027e9f7bec04afec",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "808cb3964ae94af98038",
+    "url": "/todos/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6b99b3bc3cd4d3c528e9",
+    "url": "/todos/static/css/main.00a8339d.chunk.css"
+  },
+  {
+    "revision": "808cb3964ae94af98038",
+    "url": "/todos/static/js/2.66765444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos/static/js/2.66765444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b99b3bc3cd4d3c528e9",
+    "url": "/todos/static/js/main.ee711d16.chunk.js"
+  },
+  {
+    "revision": "06f352b65e540bf1c983",
+    "url": "/todos/static/js/runtime-main.c409f8d6.js"
+  }
+]);
